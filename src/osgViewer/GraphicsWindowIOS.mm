525778387d44f3a5b239c9ff69f295c1033662eb
Colin Cochran <colin@terminaleleven.com>
2013-08-21 16:30:48 -0600

    Fixes a bug where touches are mapped to the same id.
    computeTouchId() returns the pointer address of the received touch input instead of trying to track ids with a map. This is how apple recommends to do this.

--- src/osgViewer/GraphicsWindowIOS.mm
+++ src/osgViewer/GraphicsWindowIOS.mm
@@ -165,60 +165,7 @@ typedef std::map<void*, unsigned int> TouchPointsIdMapping;
 
 - (unsigned int)computeTouchId: (UITouch*) touch 
 {
-    unsigned int result(0);
-    
-    if (!_touchPointsIdMapping) {
-        _lastTouchPointId = 0;
-        _touchPointsIdMapping = new TouchPointsIdMapping();
-    }
-    
-    switch([touch phase])
-    {
-    
-        case UITouchPhaseBegan:
-            {
-                TouchPointsIdMapping::iterator itr = _touchPointsIdMapping->find(touch);
-                // std::cout << "new: " << touch << " num: " << _touchPointsIdMapping->size() << " found: " << (itr != _touchPointsIdMapping->end()) << std::endl;
-                 
-                if (itr == _touchPointsIdMapping->end()) 
-                {
-                    (*_touchPointsIdMapping)[touch] = result = _lastTouchPointId;
-                    _lastTouchPointId++;
-                    break;
-                }
-               
-            }
-            // missing "break" by intention!
-        
-        case UITouchPhaseMoved:
-        case UITouchPhaseStationary:
-            {
-                result = (*_touchPointsIdMapping)[touch];
-            }
-            break;
-       
-        case UITouchPhaseEnded:
-        case UITouchPhaseCancelled:
-            {
-                TouchPointsIdMapping::iterator itr = _touchPointsIdMapping->find(touch);
-                // std::cout<< "remove: " << touch << " num: " << _touchPointsIdMapping->size() << " found: " << (itr != _touchPointsIdMapping->end()) << std::endl;
-                
-                if (itr != _touchPointsIdMapping->end()) {
-                    result = itr->second;
-                    _touchPointsIdMapping->erase(itr);
-                }
-                if(_touchPointsIdMapping->size() == 0) {
-                    _lastTouchPointId = 0;
-                }
-                // std::cout<< "remove: " << touch << " num: " << _touchPointsIdMapping->size() << std::endl;
-            }
-            break;
-            
-        default:
-            break;
-    }
-        
-    return result;
+    return (unsigned int)touch;
 }
 
 
